906aff76e824ec30229d0b9272119aabc986cb26
Frany-oss <francesco8bassino@gmail.com>
2022-09-08 14:58:55 -0500

    getting some index.html

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,19 @@
 
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpServer, Responder};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn index() -> impl Responder {
+    "Hello World"
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+        App::new().service(
+            // prefixes all resources and routes attached to it...
+            web::scope("/app")
+            // ...so this handles requests fot 'GET /app/index.hmtl'
+            .route("/index.html", web::get().to(index)),
+        )
     })
     .bind(("127.0.0.1", 8082))?
     .run()
