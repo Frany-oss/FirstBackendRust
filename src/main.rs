9851cdfc15c8291b3dbbae5812da9bd17aec3706
Frany-oss <francesco8bassino@gmail.com>
2022-09-09 11:19:48 -0500

    .

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ use serde::{Serialize, Deserialize};
 use std::fmt::Display;
 use std::sync::Mutex;
 
-
 #[derive(Serialize, Deserialize)]
 struct Ticket {
    id: u32,
@@ -24,6 +23,43 @@ struct ErrNoId {
 struct AppState {
    tickets: Mutex<Vec<Ticket>>,
 }
+
+// Implement Responder Trait for Ticket
+impl Responder for Ticket {
+   type Body = BoxBody;
+
+   fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
+      let res_body = serde_json::to_string(&self).unwrap();
+      
+      // Create HttpResponse and set Content Type
+      HttpResponse::Ok()
+         .content_type(ContentType::json())
+         .body(res_body)
+   }
+}
+
+// Implement ResponseError for ErrNoId
+impl ResponseError for ErrNoId {
+   fn status_code(&self) -> StatusCode {
+       StatusCode::NOT_FOUND
+   }
+
+   fn error_response(&self) -> HttpResponse<BoxBody> {
+      let body = serde_json::to_string(&self).unwrap();
+      let res = HttpResponse::new(self.status_code());
+      res.set_body(BoxBody::new(body))
+   }
+}
+
+// Implement Display for ErrNoId
+impl Display for ErrNoId {
+   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+      write!(f, "{:?}", self)
+   }
+}
+
+
+//  ------ CRUD ------
 
 // Create a ticket
 #[post("/tickets")]
@@ -135,39 +171,7 @@ async fn delete_ticket(id: web::Path<u32>, data: web::Data<AppState>) -> Result<
 }
 
 
-// Implement Responder Trait for Ticket
-impl Responder for Ticket {
-   type Body = BoxBody;
 
-   fn respond_to(self, req: &HttpRequest) -> HttpResponse<Self::Body> {
-      let res_body = serde_json::to_string(&self).unwrap();
-      
-      // Create HttpResponse and set Content Type
-      HttpResponse::Ok()
-         .content_type(ContentType::json())
-         .body(res_body)
-   }
-}
-
-// Implement ResponseError for ErrNoId
-impl ResponseError for ErrNoId {
-   fn status_code(&self) -> StatusCode {
-       StatusCode::NOT_FOUND
-   }
-
-   fn error_response(&self) -> HttpResponse<BoxBody> {
-      let body = serde_json::to_string(&self).unwrap();
-      let res = HttpResponse::new(self.status_code());
-      res.set_body(BoxBody::new(body))
-   }
-}
-
-// Implement Display for ErrNoId
-impl Display for ErrNoId {
-   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-      write!(f, "{:?}", self)
-   }
-}
 
 
 #[actix_web::main]
