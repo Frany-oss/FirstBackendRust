6cd102e368fd3838c697e691b9d2eb76316549bf
Frany-oss <francesco8bassino@gmail.com>
2022-09-09 09:33:32 -0500

    response for errores and tickets

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,57 @@
+use actix_web::{get, post, put, delete, web, App, HttpRequest, HttpResponse, HttpServer, Responder, ResponseError};
+use actix_web::http::header::ContentType;
+use actix_web::http::StatusCode;
+use actix_web::body::BoxBody;
 
-use actix_web::{web, App, HttpServer, Responder};
+use serde::{Serialize, Deserialize};
 
-async fn index() -> impl Responder {
-    "Hello World"
+use std::fmt::Display;
+use std::sync::Mutex;
+
+
+#[derive(Serialize, Deserialize)]
+struct Ticket {
+   id: u32,
+   author: String,
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(
-            // prefixes all resources and routes attached to it...
-            web::scope("/app")
-            // ...so this handles requests fot 'GET /app/index.hmtl'
-            .route("/index.html", web::get().to(index)),
-        )
-    })
-    .bind(("127.0.0.1", 8082))?
-    .run()
-    .await
+#[derive(Debug, Serialize)]
+struct ErrNoId {
+   id: u32,
+   err: String,
 }
+
+// Implement Responder Trait for Ticket
+impl Responder for Ticket {
+   type Body = BoxBody;
+
+   fn respond_to(self, req: &HttpRequest) -> HttpResponse<Self::Body> {
+      let res_body = serde_json::to_string(&self).unwrap();
+      
+      // Create HttpResponse and set Content Type
+      HttpResponse::Ok()
+         .content_type(ContentType::json())
+         .body(res_body)
+   }
+}
+
+// Implement ResponseError for ErrNoId
+impl ResponseError for ErrNoId {
+   fn status_code(&self) -> StatusCode {
+       StatusCode::NOT_FOUND
+   }
+
+   fn error_response(&self) -> HttpResponse<BoxBody> {
+      let body = serde_json::to_string(&self).unwrap();
+      let res = HttpResponse::new(self.status_code());
+      res.set_body(BoxBody::new(body))
+   }
+}
+
+// Implement Display for ErrNoId
+impl Display for ErrNoId {
+   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+      write!(f, "{:?}", self)
+   }
+}
+
